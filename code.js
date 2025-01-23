65b96b93daf3e3fb567b8a892c1eb2d4ad596a47
aPannell064 <157420768+aPannell064@users.noreply.github.com>
2025-01-22 21:40:01 -0700

    Update code.js

--- code.js
+++ code.js
@@ -9,7 +9,7 @@ function binarySearch(list, element) {
         }
         else {mid = list.length/2-1;}
 
-        /*Search list
+        /*Search the list
         Making sure that the list length goes to 1 
         ensures the first instance of the element is returned. */
         if(list.length == 1 && element == list[mid]) {
