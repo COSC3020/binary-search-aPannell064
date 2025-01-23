b54660d844e7ac97d87010ab88485bdf5f288c8d
aPannell064 <adamkpannell@gmail.com>
2025-01-22 19:09:30 -0700

    Update code.js and README

--- code.js
+++ code.js
@@ -1,3 +1,26 @@
 function binarySearch(list, element) {
-    return -1;
+    var mid,
+    index = 0;
+
+    while(list.length != 0) {
+        //Find middle index
+        if(list.length%2 == 1) {
+            mid = (list.length-1)/2;
+        }
+        else {mid = list.length/2-1;}
+
+        //Search list
+        if(element == list[mid]) {
+            index+=mid;
+            return index;
+        }
+        else if(element < list[mid]) {
+            list = list.slice(0, mid);
+        }
+        else {
+            index+=mid+1;
+            list = list.slice(mid+1, list.length);
+        }
+    }
+        return -1;
 }
